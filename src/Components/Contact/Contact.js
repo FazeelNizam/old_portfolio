9388faffce8d53d329ae2fd95cb1784f9ac75f25
FazeelNizam <nizamfazeel@gmail.com>
2023-04-24 22:44:06 +0530

    Updated contact form & image frame

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,76 +1,109 @@
-import React from "react";
-import { useState, useRef } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { RiMessengerLine } from "react-icons/ri";
-import { RiWhatsappLine } from "react-icons/ri";
-import contactImg from "../../Assets/img/contact-img.svg";
-import "./Contact.css";
-import "animate.css";
-import TrackVisibility from "react-on-screen";
-import emailjs from "emailjs-com";
+import React from 'react'
+import { useState, useRef } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { RiMessengerLine } from 'react-icons/ri'
+import { RiWhatsappLine } from 'react-icons/ri'
+import contactImg from '../../Assets/img/contact-img.svg'
+import './Contact.css'
+import 'animate.css'
+import TrackVisibility from 'react-on-screen'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
   const formInitialDetails = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    message: "",
-  };
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState("Send");
-  const [status, setStatus] = useState({});
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    message: '',
+  }
+  const [formDetails, setFormDetails] = useState(formInitialDetails)
+  const [buttonText, setButtonText] = useState('Send')
+  const [status, setStatus] = useState({})
+  const [formError, setFormError] = useState({})
+  const [isSubmit, setSubmit] = useState(false)
 
-  const form = useRef();
+  const form = useRef()
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setButtonText("Sending...");
+    e.preventDefault()
+    setFormError(validationForm(formDetails))
+    setSubmit(true)
 
-    emailjs
-      .sendForm(
-        "service_ege1l1n",
-        "template_tyky2lg",
-        form.current,
-        "dWbnuZE5il0hpfLP-"
-      )
-      .then(
-        () => {
-          setButtonText("Send");
-          setStatus({ succes: true, message: "Message sent successfully" });
-          setFormDetails(formInitialDetails);
-        },
-        (error) => {
-          setStatus({
-            succes: false,
-            message: "Something went wrong, please try again later.",
-          });
-          console.log(error.text);
-        }
-      );
-  };
+    if (Object.keys(formError).length === 0 && isSubmit) {
+      setButtonText('Sending...')
+
+      emailjs
+        .sendForm(
+          'service_ege1l1n',
+          'template_tyky2lg',
+          form.current,
+          'dWbnuZE5il0hpfLP-'
+        )
+        .then(
+          () => {
+            setButtonText('Send')
+            setStatus({ succes: true, message: 'Message sent successfully' })
+            setFormDetails(formInitialDetails)
+          },
+          (error) => {
+            setStatus({
+              succes: false,
+              message: 'Something went wrong, please try again later.',
+            })
+            console.log(error.text)
+          }
+        )
+    }
+  }
 
   const onFormUpdate = (category, value) => {
     setFormDetails({
       ...formDetails,
       [category]: value,
-    });
-  };
+    })
+  }
+
+  const validationForm = (value) => {
+    const errors = {}
+    const emailPattern =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if (!value.firstName) {
+      errors.firstName = 'Please Enter First Name'
+    }
+    if (!value.lastName) {
+      errors.lastName = 'Please Enter Last Name'
+    }
+    if (!value.email) {
+      errors.email = 'Please Enter Email'
+    } else if (!emailPattern.test(value.email)) {
+      errors.email = 'Enter Valid Email'
+    }
+    if (!value.phone) {
+      errors.phone = 'Please Enter Phone Number'
+    }
+    if (!value.message) {
+      errors.message = 'Please Enter Message'
+    }
+
+    return errors
+  }
 
   return (
     <>
-      <section className="contact" id="contact">
+      <section className='contact' id='contact'>
         <Container>
-          <Row className="align-items-center">
+          <Row className='align-items-center'>
             <Col size={12} md={6}>
               <TrackVisibility>
                 {({ isVisible }) => (
                   <img
                     className={
-                      isVisible ? "animate__animated animatezoomIn" : ""
+                      isVisible ? 'animate__animated animatezoomIn' : ''
                     }
                     src={contactImg}
-                    alt="Contact Us"
+                    alt='Contact Us'
                   />
                 )}
               </TrackVisibility>
@@ -79,18 +112,18 @@ const Contact = () => {
               <h2>Get In Touch</h2>
               <Row>
                 <Col>
-                  <div className="contact-icon">
-                    <a href="http://m.me/FazeelNizam00" target="_blank">
+                  <div className='contact-icon'>
+                    <a href='http://m.me/FazeelNizam00' target='_blank'>
                       <RiMessengerLine size={25} />
                       <span>Messanger</span>
                     </a>
                   </div>
                 </Col>
                 <Col>
-                  <div className="contact-icon">
+                  <div className='contact-icon'>
                     <a
-                      href="https://api.whatsapp.com/send?phone=+94768114000"
-                      target="_blank"
+                      href='https://api.whatsapp.com/send?phone=+94768114000'
+                      target='_blank'
                     >
                       <RiWhatsappLine size={25} />
                       <span>Whatsapp</span>
@@ -100,61 +133,69 @@ const Contact = () => {
               </Row>
               <form onSubmit={handleSubmit} ref={form}>
                 <Row>
-                  <Col size={12} sm={6} className="px-1">
+                  <Col size={12} sm={6} className='px-1'>
                     <input
-                      type="text"
-                      name="firstname"
+                      type='text'
+                      name='firstname'
                       value={formDetails.firstName}
-                      placeholder="First Name"
+                      placeholder='First Name'
                       onChange={(e) =>
-                        onFormUpdate("firstName", e.target.value)
+                        onFormUpdate('firstName', e.target.value)
                       }
                     />
+                    <span className='text-danger'>{formError.firstName} </span>
                   </Col>
-                  <Col size={12} sm={6} className="px-1">
+                  <Col size={12} sm={6} className='px-1'>
                     <input
-                      type="text"
-                      name="lastname"
+                      type='text'
+                      name='lastname'
                       value={formDetails.lastName}
-                      placeholder="Last Name"
-                      onChange={(e) => onFormUpdate("lastName", e.target.value)}
+                      placeholder='Last Name'
+                      onChange={(e) => onFormUpdate('lastName', e.target.value)}
                     />
+                    <span className='text-danger'>{formError.lastName} </span>
                   </Col>
-                  <Col size={12} sm={6} className="px-1">
+                  <Col size={12} sm={6} className='px-1'>
                     <input
-                      type="email"
-                      name="email"
+                      type='email'
+                      name='email'
                       value={formDetails.email}
-                      placeholder="Email Address"
-                      onChange={(e) => onFormUpdate("email", e.target.value)}
+                      placeholder='Email Address'
+                      onChange={(e) => onFormUpdate('email', e.target.value)}
                     />
+                    <span className='text-danger'>{formError.email} </span>
                   </Col>
-                  <Col size={12} sm={6} className="px-1">
+                  <Col size={12} sm={6} className='px-1'>
                     <input
-                      type="tel"
-                      name="tel"
+                      type='tel'
+                      name='tel'
                       value={formDetails.phone}
-                      placeholder="Phone No."
-                      onChange={(e) => onFormUpdate("phone", e.target.value)}
+                      placeholder='Phone No.'
+                      onChange={(e) => onFormUpdate('phone', e.target.value)}
                     />
+                    <span className='text-danger'>{formError.phone} </span>
                   </Col>
-                  <Col size={12} className="px-1">
+                  <Col size={12} className='px-1'>
                     <textarea
-                      rows="6"
-                      name="message"
+                      rows='6'
+                      name='message'
                       value={formDetails.message}
-                      placeholder="Message"
-                      onChange={(e) => onFormUpdate("message", e.target.value)}
+                      placeholder='Message'
+                      onChange={(e) => onFormUpdate('message', e.target.value)}
                     ></textarea>
-                    <button type="submit" value="Send">
-                      <span>{buttonText}</span>
-                    </button>
+                    <span className='text-danger'>{formError.message} </span>
+
+                    <Col size={12}>
+                      <button type='submit' value='Send'>
+                        <span>{buttonText}</span>
+                      </button>
+                    </Col>
                   </Col>
                   {status.message && (
                     <Col>
                       <p
                         className={
-                          status.success === false ? "danger" : "success"
+                          status.success === false ? 'danger' : 'success'
                         }
                       >
                         {status.message}
@@ -168,10 +209,10 @@ const Contact = () => {
         </Container>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Contact;
+export default Contact
 
 {
   /* <a href="mailto:fazeelmohamad5@gmail.com" target="_blank">Send a Message</a> */
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import { ProjectCard } from "./ProjectCard";
-import "./Projects.css";
-import "./sky.scss";
-import "animate.css";
-import Frame from "./Frame";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import { projects } from "./ProjectData.js";
-import UI from "../../Assets/img/Illustrations/Breadchoice.png";
-import CGU from "../../Assets/img/Illustrations/CGUToken.jpg";
-import CRT from "../../Assets/img/Illustrations/Circuit.PNG";
-import HLX from "../../Assets/img/Illustrations/Helix.PNG";
-import D3 from "../../Assets/img/Illustrations/3D.PNG";
-import CRE5 from "../../Assets/img/Illustrations/CRE05.PNG";
-import MTR from "../../Assets/img/Illustrations/Motor.PNG";
+import React, { useState } from 'react'
+import { Container, Row, Col, Tab, Nav } from 'react-bootstrap'
+import { ProjectCard } from './ProjectCard'
+import './Projects.css'
+import './sky.scss'
+import 'animate.css'
+import Frame from './Frame'
+import ImageList from '@mui/material/ImageList'
+import ImageListItem from '@mui/material/ImageListItem'
+import { projects } from './ProjectData.js'
+import UI from '../../Assets/img/Illustrations/Breadchoice.png'
+import CGU from '../../Assets/img/Illustrations/CGUToken.jpg'
+import CRT from '../../Assets/img/Illustrations/Circuit.PNG'
+import HLX from '../../Assets/img/Illustrations/Helix.PNG'
+import D3 from '../../Assets/img/Illustrations/3D.PNG'
+import CRE5 from '../../Assets/img/Illustrations/CRE05.PNG'
+import MTR from '../../Assets/img/Illustrations/Motor.PNG'
 
 function srcset(image, size, rows = 1, cols = 1) {
   return {
@@ -22,53 +22,56 @@ function srcset(image, size, rows = 1, cols = 1) {
     srcSet: `${image}?w=${size * cols}&h=${
       size * rows
     }&fit=crop&auto=format&dpr=2 2x`,
-  };
+  }
 }
 
 const Projects = () => {
-  const [selectedImg, setSelectedImg] = useState(null);
+  const [selectedImg, setSelectedImg] = useState(null)
+  selectedImg
+    ? (document.body.style.overflow = 'hidden')
+    : (document.body.style.overflow = 'auto')
   return (
     <>
-      <section className="project" id="projects">
-        <div className="sky-container">
-          <div class="star1"></div>
-          <div class="star2"></div>
-          <div class="star3"></div>
+      <section className='project' id='projects'>
+        <div className='sky-container'>
+          <div class='star1'></div>
+          <div class='star2'></div>
+          <div class='star3'></div>
         </div>
 
-        <Container className="tab-container">
+        <Container className='tab-container'>
           <Row>
             <Col size={12}>
               <h2>Projects</h2>
               <p>A small gallery of my recent projects.</p>
-              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Tab.Container id='projects-tabs' defaultActiveKey='first'>
                 <Nav
-                  variant="pills"
-                  className="nav-pills mb-5 justify-content-center align-items-center"
-                  id="pills-tab"
+                  variant='pills'
+                  className='nav-pills mb-5 justify-content-center align-items-center'
+                  id='pills-tab'
                 >
                   <Nav.Item>
-                    <Nav.Link eventKey="first">Web Devs</Nav.Link>
+                    <Nav.Link eventKey='first'>Web Devs</Nav.Link>
                   </Nav.Item>
                   <Nav.Item>
-                    <Nav.Link eventKey="second">Illustrations</Nav.Link>
+                    <Nav.Link eventKey='second'>Illustrations</Nav.Link>
                   </Nav.Item>
                 </Nav>
                 <Tab.Content>
-                  <Tab.Pane eventKey="first">
+                  <Tab.Pane eventKey='first'>
                     <Row>
                       {projects.map((project, index) => {
-                        return <ProjectCard key={index} {...project} />;
+                        return <ProjectCard key={index} {...project} />
                       })}
                     </Row>
                   </Tab.Pane>
-                  <Tab.Pane eventKey="second">
+                  <Tab.Pane eventKey='second'>
                     <Row>
                       <Col size={12}>
-                        <div className="gallery">
+                        <div className='gallery'>
                           <ImageList
                             sx={{ width: 1, height: 0.7 }}
-                            variant="quilted"
+                            variant='quilted'
                             cols={4}
                             rowHeight={110}
                           >
@@ -80,7 +83,7 @@ const Projects = () => {
                                 layoutId={item.id}
                               >
                                 <img
-                                  className="gallery-img"
+                                  className='gallery-img'
                                   {...srcset(
                                     item.img,
                                     121,
@@ -88,7 +91,7 @@ const Projects = () => {
                                     item.cols
                                   )}
                                   alt={item.title}
-                                  loading="lazy"
+                                  loading='lazy'
                                   onClick={() => setSelectedImg(item.img)}
                                 />
                               </ImageListItem>
@@ -111,58 +114,58 @@ const Projects = () => {
         </Container>
       </section>
     </>
-  );
-};
+  )
+}
 
 const imageData = [
   {
     id: 1,
     img: UI,
-    title: "Breakfast",
+    title: 'Breakfast',
     rows: 2.5,
     cols: 4,
   },
   {
     id: 2,
     img: MTR,
-    title: "Hats",
+    title: 'Hats',
     cols: 1,
   },
   {
     id: 3,
     img: CGU,
-    title: "Honey",
+    title: 'Honey',
     rows: 2,
     cols: 3,
   },
   {
     id: 4,
     img: HLX,
-    title: "Burger",
+    title: 'Burger',
     rows: 2,
     cols: 1,
   },
   {
     id: 5,
     img: CRE5,
-    title: "Burger",
+    title: 'Burger',
     rows: 2,
     cols: 1,
   },
   {
     id: 6,
     img: CRT,
-    title: "Burger",
+    title: 'Burger',
     rows: 2,
     cols: 2,
   },
   {
     id: 7,
     img: D3,
-    title: "Burger",
+    title: 'Burger',
     rows: 1,
     cols: 1,
   },
-];
+]
 
-export default Projects;
+export default Projects
